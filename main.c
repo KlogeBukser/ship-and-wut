9a304d3c0ae6a3a7ff076b9c8dcfd549cecb5ce6
KlogeBukser <75204737+KlogeBukser@users.noreply.github.com>
2020-11-30 23:27:55 +1100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ship and stuff
+//
+//  Created by Victoria Chen on 29/11/20.
+//  Copyright © 2020 cuteppl. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
