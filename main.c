429f2208447bd78f2bd62a3130ac9699051a611b
KlogeBukser <75204737+KlogeBukser@users.noreply.github.com>
2020-12-07 03:41:25 +1100

    Update main.c

--- main.c
+++ main.c
@@ -17,6 +17,7 @@
 // define constants
 #define ROW_SIZE    10
 #define COLUMN_SIZE 10
+#define SHIP_COUNT  5
 
 // direction of the ships
 #define HORIZONTAL  0
@@ -44,18 +45,41 @@ int is_illegal(short row, short column, short row_size, short column_size);
 void render(void);
 void graph(short column_size, short row_size, char *shot_grid, char *ship_grid);
 char *create_shotgrid(short row_size, short column_size);
-char *read_ship_grid(short row_size, short column_size, int *ship_count);
-int take_shot(short row_size, short column_size, char *shot_grid, char *ship_grid);
+char *place_ships(short row_size, short column_size, int ship_count);
+int test_shot(int *result, short row_size, short column_size, char *ship_grid);
+
+
+int *shoot_ship()
+{
+    int row;
+    char column;
+    int *choice;
+    choice = (int *) malloc(2*sizeof(int));
+    while(1){
+        printf("Where will you shoot? (1-10 / a-j)\n");
+        scanf("%d %c", &row, &column);
+        if (row >= 1 && row <= 10 && column >=97 && column < 107){
+            choice[0] = row-1;
+            choice[1] = column-97;
+            break;
+        }
+        printf("Invalid input!!!\n");
+    }
+    return choice;
+}
+
 
 int main(int argc, const char * argv[]) {
     
-    int ship_count = 0;
+    int ship_count = SHIP_COUNT, *shot, result;
     char *shotgrid, *shipgrid;
     
     //debug
     shotgrid = create_shotgrid(ROW_SIZE, COLUMN_SIZE);
-    shipgrid = read_ship_grid(ROW_SIZE, COLUMN_SIZE, &ship_count);
-    take_shot(ROW_SIZE, COLUMN_SIZE, shotgrid, shipgrid);
+    shipgrid = place_ships(ROW_SIZE, COLUMN_SIZE, ship_count);
+    shot = shoot_ship();
+    result = test_shot(shot, ROW_SIZE, COLUMN_SIZE, shipgrid);
+    
     graph(COLUMN_SIZE, ROW_SIZE, shotgrid, shipgrid);
     
     printf("Dumme svin!\n");
@@ -110,7 +134,6 @@ int is_illegal(short row, short column, short row_size, short column_size) {
 // deal with shots
 
 char *create_shotgrid(short row_size, short column_size) {
-    
     char *shot_grid; int i;
     
     shot_grid = (char *)malloc(row_size*(column_size)*sizeof(char));
@@ -122,37 +145,16 @@ char *create_shotgrid(short row_size, short column_size) {
     return shot_grid;
 }
 
-int test_shot(short row, short column, short row_size, short column_size, char *ship_grid);
 
-int take_shot(short row_size, short column_size, char *shot_grid, char *ship_grid) {
-    
-    short row, column; int result = INPUT_ERR_INVALID_CELL, i;
-    
-    for (i=0;i<5;i++) {
-        printf("give a shot, in the format of [row, column] \n");
-        if (2 == scanf("[%hd,%hd] ",&row, &column)) {
-            if (1 == shot_grid[get_grid(row, column, column_size)]) {
-                printf("You've shot here, try again you stupid fool! \n");
-            }
-            shot_grid[get_grid(row, column, column_size)] = 1;
-            result = test_shot(row, column, row_size, column_size, ship_grid);
-            break;
-        }
-        else {
-            printf("Not a cell, please try again, %d times remaining \n", i);
-        }
-    }
-     return result;
-}
 
-int test_shot(short row, short column, short row_size, short column_size, char *ship_grid) {
+int test_shot(int* choice, short row_size, short column_size, char *ship_grid) {
     
-    if (is_illegal(row, column, column_size, row_size)) {
+    if (is_illegal(choice[0], choice[1], column_size, row_size)) {
         printf("Your shot is not in the grid, stupid fwak \n");
         return GAME_ERR_ILLEGAL_SHOT;
     }
     
-    if (1 == ship_grid[get_grid(row, column, column_size)]) {
+    if ('1' == ship_grid[get_grid(choice[0], choice[1], column_size)]) {
         return HIT;
     }
     return MISS;
@@ -164,7 +166,7 @@ int get_grid(short row, short column, int column_size) {
 }
 
 // get the grid for ships
-char *read_ship_grid(short row_size, short column_size, int *ship_count) {
+char *place_ships(short row_size, short column_size, int ship_count) {
     char *ship_grid; int i; short row, column, length, is_vertical;
     
     ship_grid = (char *)malloc(row_size*(column_size)*sizeof(char));
@@ -175,26 +177,23 @@ char *read_ship_grid(short row_size, short column_size, int *ship_count) {
     }
     
     printf("give a ship, in the format of [row, column, length, is_vertical] \n");
-    while (4 == scanf("[%hd,%hd,%hd,%hd] ",&row, &column, &length, &is_vertical)) {
-        for (i=0;i<length;i++) {
-            if (is_illegal(row, column, row_size, column_size)) {
-                printf("illegal bitch!!! \n");
-                break;
-            }
-            ship_grid[get_grid(row, column, column_size)] = 1;
-            if (is_vertical) {
-                column++;
-            }
-            else {
-                row++;
-            }
-            (*ship_count)++;
-        }
-    }
-    
-    // if no ship
-    if (0 == *ship_count) {
-        printf("No fucking ships you idiot!! \n");
+    for (i=0;i<ship_count;i++) {
+         if (4 == scanf("[%hd,%hd,%hd,%hd] ",&row, &column, &length, &is_vertical)) {
+               for (i=0;i<length;i++) {
+                   if (is_illegal(row, column, row_size, column_size)) {
+                       printf("illegal bitch!!! \n");
+                       break;
+                   }
+                   ship_grid[get_grid(row, column, column_size)] = 1;
+                   if (is_vertical) {
+                       column++;
+                   }
+                   else {
+                       row++;
+                   }
+
+               }
+         }
     }
     
     return ship_grid;
